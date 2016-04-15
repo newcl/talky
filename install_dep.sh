d232c39f415392790085a2dc8f0c5ba7839516d7
陈亮 <chenliang.info@gmail.com>
2016-04-15 15:27:46 +0800

    use built boost library

--- install_dep.sh
+++ install_dep.sh
@@ -1,6 +1,10 @@
-wget http://downloads.sourceforge.net/project/boost/boost/1.57.0/boost_1_57_0.tar.bz2
-tar --bzip2 -xf boost_1_57_0.tar.bz2
-cd boost_1_57_0
-./bootstrap.sh
-./b2 install
-cd -
+# wget http://downloads.sourceforge.net/project/boost/boost/1.57.0/boost_1_57_0.tar.bz2
+# tar --bzip2 -xf boost_1_57_0.tar.bz2
+# cd boost_1_57_0
+# ./bootstrap.sh
+# ./b2 install
+# cd -
+
+# boost takes too long to download compile & install
+
+sudo apt-get install libboost-dev
