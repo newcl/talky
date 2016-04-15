c618c80782f5c1a4671308c2248100bdd88acff4
陈亮 <chenliang.info@gmail.com>
2016-04-15 14:18:38 +0800

    work around compiling c++ in a java project

--- build.sh
+++ build.sh
@@ -9,5 +9,5 @@ flex talky.l
 cd ..
 
 # -ll for mac os, others should use -lfl if flex is installed correctly
-# g++ -std=c++11 ./parser/src/parser.cpp ./bison/talky.tab.c ./flex/lex.yy.c -ll -o talky
-g++ -w -std=c++11 -lboost_program_options -lboost_filesystem -lboost_system -ll -lstdc++ -o talky ./bison/talky.tab.c ./flex/lex.yy.c ./parser/src/parser.cpp 
+#g++ -w -std=c++11 -lboost_program_options -lboost_filesystem -lboost_system -ll -lstdc++ -o talky ./bison/talky.tab.c ./flex/lex.yy.c ./parser/src/parser.cpp 
+g++ -w -lboost_program_options -lboost_filesystem -lboost_system -ll -lstdc++ -o talky ./bison/talky.tab.c ./flex/lex.yy.c ./parser/src/parser.cpp 
